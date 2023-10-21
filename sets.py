9abed77be7ee0f6a3c793f765e085a36ee0df82a
greatbrain <erickfania@gmail.com>
2023-10-21 16:42:07 -0400

    Add: practices and theory about sets

--- sets.py
+++ sets.py
@@ -1,23 +1,10 @@
-# Small program to delete duplicate data. This using sets.
-def set_data():
-    numbers = [5, 5, 25, 10, 1, 2, 2]
-    return numbers
+''' Understanding what Python Set data 
+type is deeply and how to apply it into real world problems.
+Author: Mr. Fana 2023 '''
 
-
-def clean_data():
-    return list(set(set_data()))
-
-
-def get_data():
-    return clean_data()
-
-#print(get_data())
-
-
-'''   '''
-
+#Finding common items of two sets by comparing 'em
 def compare_users():
-    #Compare data of two sets to find common items
+
     sytem_users_2022 = {"Bobby", 
                         "Ramon", 
                         "Julieta", 
@@ -39,15 +26,56 @@ def compare_users():
 #print(compare_users())
 
 
-
-'''   '''
-
 # Makin' a copy of a set:
-def copy_set():
-    set_1 = {1, 2, 5}
-    print("I am the original set:", " ", set_1)
-    
-    set_2 = set_1.copy()
-    print("I am the copy set:", " ", set_2)
+def get_copy():
+    set_1 = {1, 2, 3, 4}
+    print("I'm the original set", " ", set_1)
 
-copy_set()
+
+    set_2 = set_1.copy()
+    print("I'm the copy set", " ", set_2, "\n") 
+    # As we can see here, is exactly the same data duplicated
+    # and it's shown below throw the memory address in buffer of each variable:
+
+    print("I am the memory address of the original set:", "",  id(set_1))
+    print("I am the memory address of the copy set:", "", id(set_2))
+
+#get_copy()
+
+#Getting the difference of two sets:
+def get_difference():
+    set_a = {1, 22, 8, 4}
+    set_b = {1, 2, 3, 4, 5, 6}
+    set_c = set_b.intersection(set_a) 
+
+    return set_c  # It should be '1 and 4'
+
+#print(get_difference())
+
+
+
+
+
+
+
+''' Practice '''
+''' Checking for Anagrams: Use sets to check if two strings are anagrams of each other by comparing the sets of characters in each string. '''
+
+def is_anagram(first_word=str, second_word=str):
+    first_word = set(first_word)
+    second_word = set(second_word)
+
+    if (first_word ^ second_word):       
+       return False    
+    return True
+
+print(is_anagram("ROMA", "OMZAR"), "\n")
+print(is_anagram("IEVL", "LIVE"), "\n")
+
+'''As we can see in the above code, the function 'is_anagram' is executed twice
+the first one recives two str parameter, a little bit differents and should return False because we can not get a anagram.
+The second function calling recives the same string twice just unordered, but they are the same. And the is_anagram function 
+should return True.'''
+
+
+
